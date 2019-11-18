4f0246ff30d4fd90110b451d4505184d80500292
Irho <irhad.saric@hotmail.com>
2019-11-18 03:02:40 +0100

    Entities for teams, projects and other many-to-many tables

---
+++ src/main/java/ba/unsa/pmf/pragma/db/entity/Team.java
@@ -0,0 +1,54 @@
+package ba.unsa.pmf.pragma.db.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "team")
+public class Team extends BaseEntity{
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "logo", nullable = false)
+    private byte[] logo;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
