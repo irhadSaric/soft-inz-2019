bf21f2cd93375f3a8496e5a29883841dad099e48
Irho <irhad.saric@hotmail.com>
2019-11-24 20:24:48 +0100

    get team members

--- src/main/java/ba/unsa/pmf/pragma/db/repository/UserTeamRepository.java
+++ src/main/java/ba/unsa/pmf/pragma/db/repository/UserTeamRepository.java
@@ -1,9 +1,28 @@
 package ba.unsa.pmf.pragma.db.repository;
 
+import ba.unsa.pmf.pragma.db.entity.Permission;
+import ba.unsa.pmf.pragma.db.entity.Team;
+import ba.unsa.pmf.pragma.db.entity.User;
 import ba.unsa.pmf.pragma.db.entity.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserTeamRepository extends JpaRepository<UserTeam, Long>{
+public interface UserTeamRepository extends JpaRepository<UserTeam,Long> {
+    @Query(value =
+            "select t.team from UserTeam t where t.user.id = :userId"
+    )
+    List<Team> getTeamsForUser(@Param("userId") Long userId);
+
+    @Query(value ="select u " +
+            "from User u " +
+            "inner join UserTeam ut on ut.user.id = u.id "+
+            "inner join Team t on t.id = ut.team.id "+
+            "where t.id = :teamId"
+    )
+    List<User> getTeamMembers(@Param("teamId") Long teamId);
 }
---
+++ src/main/java/ba/unsa/pmf/pragma/service/UserTeamService.java
@@ -0,0 +1,22 @@
+package ba.unsa.pmf.pragma.service;
+
+import ba.unsa.pmf.pragma.db.entity.Team;
+import ba.unsa.pmf.pragma.db.entity.User;
+import ba.unsa.pmf.pragma.db.repository.UserTeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserTeamService {
+
+    @Autowired
+    UserTeamRepository userTeamRepository;
+
+    public List<Team> getTeamsForUser(Long user_id) {
+      return userTeamRepository.getTeamsForUser(user_id);
+    }
+
+    public List<User> getTeamMembers(Long team_id) { return userTeamRepository.getTeamMembers(team_id); }
+}
