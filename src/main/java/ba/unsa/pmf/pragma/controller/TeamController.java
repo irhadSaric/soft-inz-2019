6750ebcf8db5e461aa71a158912316ec5dca3ed8
Irho <irhad.saric@hotmail.com>
2019-11-30 23:39:35 +0100

    @PutMapping("/api/user/profile/{id}/upload") removed @PostMapping("/{id}/upload") added @GetMapping("/{id}/logo") added

--- src/main/java/ba/unsa/pmf/pragma/controller/TeamController.java
+++ src/main/java/ba/unsa/pmf/pragma/controller/TeamController.java
@@ -11,8 +11,10 @@ import ba.unsa.pmf.pragma.service.dtos.UserTeamResponse;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -78,5 +80,15 @@ public class TeamController {
             @RequestParam(name = "accept") @NotNull Boolean accept
     ) throws NotFoundException {
         return userTeamService.respondToPendingInvite(userId, teamId, accept);
+    }
+
+    @PostMapping("/{id}/upload")
+    public void changeLogo(@RequestParam("file") MultipartFile file, @PathVariable Long id) throws NotFoundException, IOException {
+        teamService.uploadLogo(id, file);
+    }
+
+    @GetMapping("/{id}/logo")
+    public byte[] getLogo(@PathVariable Long id) throws NotFoundException {
+        return teamService.getLogo(id);
     }
 }
