b4f57eb80f8173b15e20c24977acfdee0f058e4b
Imrana <imranahorozovic@gmail.com>
2019-12-01 22:03:27 +0100

    initial commit

--- src/main/java/ba/unsa/pmf/pragma/db/repository/RepositoryBase.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.pmf.pragma.db.repository;
-
-// FIXME, malek.chahin: Remove if not needed?
-public class RepositoryBase {
-
-}
--- src/main/java/ba/unsa/pmf/pragma/util/JsonUtils.java
+++
@@ -1,8 +0,0 @@
-package ba.unsa.pmf.pragma.util;
-
-public class JsonUtils {
-
-	private JsonUtils() {
-		throw new IllegalStateException("Utility class");
-	}
-}
